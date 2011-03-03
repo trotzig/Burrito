d90cada365e4613c6ed67d90b68f9f0008b6fc60
mikcla <mikcla@pc0629.kanal5.local>
2011-03-03 13:34:41 +0100

    Sitelets: Better support for sitelets with javascript

--- src/main/resources/burrito/public/burrito-sitelets.js
+++ src/main/resources/burrito/public/burrito-sitelets.js
@@ -1,14 +1,70 @@
 
+function BurritoUtils() {
+	var self = this;
+
+	self.didRunOnceIdentifiers = new Array();
+
+	self.jsFilesLoaded = new Array();
+	self.jsFilesStartedLoading = new Array();
+	self.jsFilesPendingCallbacks = new Array();
+
+	this.runOnce = function(identifier, callback) {
+		if (!self.didRunOnceIdentifiers[identifier]) {
+			self.didRunOnceIdentifiers[identifier] = true;
+			callback();
+		}
+	}
+
+	this.runWithJavascriptFile = function(jsFileUrl, callback) {
+		if (self.jsFilesLoaded[jsFileUrl]) {
+			callback();
+		}
+		else {
+			var pendingCallbacks = self.jsFilesPendingCallbacks[jsFileUrl];
+			if (!pendingCallbacks) {
+				pendingCallbacks = new Array();
+				self.jsFilesPendingCallbacks[jsFileUrl] = pendingCallbacks;
+			}
+			pendingCallbacks.push(callback);
+
+			if (!self.jsFilesStartedLoading[jsFileUrl]) {
+				self.jsFilesStartedLoading[jsFileUrl] = true;
+
+				$.getScript(jsFileUrl, function() {
+					self.jsFilesLoaded[jsFileUrl] = true;
+
+					for (var i = 0; i < pendingCallbacks.length; i++) {
+						pendingCallbacks[i]();
+					}
+				});
+			}
+		}
+	}
+
+	this.getClassValue = function(element, prefix) {
+		var classes = element.attr('class').split(' ');
+		for (var i = 0; i < classes.length; i++) {
+			var c = classes[i];
+			if (c.length >= prefix.length && c.substring(0, prefix.length) == prefix) {
+				return c.substring(prefix.length);
+			}
+		}
+		return null;
+	}
+}
+
+var burritoUtils = new BurritoUtils();
+
 function BurritoSitelets() {
-	var object = this;
+	var self = this;
 
 	var adminControlsClass = 'sitelet-admin-controls';
 
-	object.onNewSiteletContentCallbacks = new Array();
+	self.onNewSiteletContentCallbacks = new Array();
 
 	this.registerLiveBox = function(siteIdentifier, boxId) {
 		burritoFeeds.registerHandler('burrito:sitelet-box:' + siteIdentifier + '|' + boxId, function(message) {
-			object.handleIncomingUpdate(boxId, eval('(' + message + ')'));
+			self.handleIncomingUpdate(boxId, eval('(' + message + ')'));
 		});
 	}
 
@@ -18,7 +74,7 @@ function BurritoSitelets() {
 		var box = $('#sitelet-box-' + boxId);
 
 		box.find('.sitelet').each(function() {
-			var id = object.getClassValue($(this), 'sitelet-properties-id-');
+			var id = burritoUtils.getClassValue($(this), 'sitelet-properties-id-');
 			for (var i = 0; i < sitelets.length; i++) {
 				if (id == sitelets[i].id) return;
 			}
@@ -36,14 +92,14 @@ function BurritoSitelets() {
 			var siteletWrapper = box.find('.sitelet.' + idClassName);
 
 			if (siteletWrapper.length) {
-				object.placeSiteletWrapper(box, previousWrapper, siteletWrapper, idClassName);
+				self.placeSiteletWrapper(box, previousWrapper, siteletWrapper, idClassName);
 			}
 
 			if (sitelet.html) {
 				var hasNewContent;
 
 				if (siteletWrapper.length) {
-					var existingVersion = object.getClassValue(siteletWrapper, 'sitelet-version-');
+					var existingVersion = burritoUtils.getClassValue(siteletWrapper, 'sitelet-version-');
 					hasNewContent = sitelet.version > existingVersion;
 					if (hasNewContent) {
 						siteletWrapper.html(sitelet.html);
@@ -54,24 +110,24 @@ function BurritoSitelets() {
 				else {
 					hasNewContent = true;
 					siteletWrapper = '<div style="display: none" class="sitelet ' + idClassName + ' sitelet-version-' + sitelet.version + '">' + sitelet.html + '</div>';
-					object.placeSiteletWrapper(box, previousWrapper, siteletWrapper, idClassName);
+					self.placeSiteletWrapper(box, previousWrapper, siteletWrapper, idClassName);
 					siteletWrapper = box.find('.sitelet.' + idClassName);
 					siteletWrapper.slideDown(500);
 				}
 
 				if (hasNewContent) {
-					for (var j = 0; j < object.onNewSiteletContentCallbacks.length; j++) {
-						var callback = object.onNewSiteletContentCallbacks[j];
+					for (var j = 0; j < self.onNewSiteletContentCallbacks.length; j++) {
+						var callback = self.onNewSiteletContentCallbacks[j];
 						callback(siteletWrapper);
 					}
 				}
 			}
-			else if (sitelet.version > object.getClassValue(siteletWrapper, 'sitelet-version-')) {
-				if (!object.boxPollTimeout) {
+			else if (sitelet.version > burritoUtils.getClassValue(siteletWrapper, 'sitelet-version-')) {
+				if (!self.boxPollTimeout) {
 					alert("polling for box contents soon");
-					object.boxPollTimeout = setTimeout(function() {
+					self.boxPollTimeout = setTimeout(function() {
 						alert("polling for box contents now");
-						object.boxPollTimeout = false;
+						self.boxPollTimeout = false;
 						$.ajax({
 							url: '/burrito/sitelets/box/' + boxId + '/poll',
 							dataType: "jsonp",
@@ -79,7 +135,7 @@ function BurritoSitelets() {
 								if (json.status == 'error') {
 									throw("Error response from feed server: " + json.message);
 								}
-								object.handleIncomingUpdate(boxId, json);
+								self.handleIncomingUpdate(boxId, json);
 							}
 						});
 					}, 100 + Math.floor(60 * 1000 * Math.random()));
@@ -108,40 +164,40 @@ function BurritoSitelets() {
 	}
 
 	this.onNewSiteletContent = function(callback) {
-		object.onNewSiteletContentCallbacks.push(callback);
+		self.onNewSiteletContentCallbacks.push(callback);
 	}
 
 	this.enableAdminControls = function() {
 		$('.' + adminControlsClass).live('click', function() {
-			var siteletPropertiesId = object.getClassValue($(this), 'sitelet-properties-id-');
+			var siteletPropertiesId = burritoUtils.getClassValue($(this), 'sitelet-properties-id-');
 
 			$(this).fadeOut(function() {
 				$(this).fadeIn();
 			});
 
-			object.refreshSitelet(siteletPropertiesId)
+			self.refreshSitelet(siteletPropertiesId)
 		});
 
 		$('.sitelet').live('mouseover mouseout', function(event) {
 			if (event.type == 'mouseover') {
-				if (!object.adminControlsTimeout) {
-					var siteletPropertiesId = object.getClassValue($(this), 'sitelet-properties-id-');
+				if (!self.adminControlsTimeout) {
+					var siteletPropertiesId = burritoUtils.getClassValue($(this), 'sitelet-properties-id-');
 
 					if (!$('.' + adminControlsClass + '.sitelet-properties-id-' + siteletPropertiesId).length) {
 						var offset = $(this).offset();
-						object.adminControlsTimeout = setTimeout(function() {
+						self.adminControlsTimeout = setTimeout(function() {
 							$('.' + adminControlsClass).remove();
 							$('body').append('<div class="' + adminControlsClass + ' sitelet-properties-id-' + siteletPropertiesId + '" style="top: ' + offset.top + 'px; left: ' + offset.left + 'px; position: absolute">Refresh</div>');
-							object.adminControlsTimeout = false;
+							self.adminControlsTimeout = false;
 						}, 1000);
 					}
 				}
 			}
 			else {
-				if (object.adminControlsTimeout) {
-					clearTimeout(object.adminControlsTimeout);
+				if (self.adminControlsTimeout) {
+					clearTimeout(self.adminControlsTimeout);
 
-					object.adminControlsTimeout = false;
+					self.adminControlsTimeout = false;
 				}
 			}
 		});
@@ -151,15 +207,8 @@ function BurritoSitelets() {
 		});
 	}
 
-	this.getClassValue = function(element, prefix) {
-		var classes = element.attr('class').split(' ');
-		for (var i = 0; i < classes.length; i++) {
-			var c = classes[i];
-			if (c.length >= prefix.length && c.substring(0, prefix.length) == prefix) {
-				return c.substring(prefix.length);
-			}
-		}
-		return null;
+	this.runOnceForSiteletVersion = function(siteletPropertiesId, renderedVersion, callback) {
+		burritoUtils.runOnce('burrito-sitelet-' + siteletPropertiesId + '-version-' + renderedVersion, callback);
 	}
 }
 
