b881da1d850ad912826ba6092914e75c64c96bc3
henper <henric.persson@gmail.com>
2012-06-26 12:56:07 +0200

    Added support for adding ChangeHandler and ValueChangeHandler using the same method in CrudInputFieldWrapper.
    Change-Id: I57b25ceb38dd64f0978e2297c719316e8ffc30fd

--- src/main/java/burrito/client/crud/input/CrudInputFieldWrapper.java
+++ src/main/java/burrito/client/crud/input/CrudInputFieldWrapper.java
@@ -6,6 +6,9 @@ import burrito.client.widgets.validation.HasValidators;
 
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.HasChangeHandlers;
+import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SimplePanel;
@@ -71,10 +74,20 @@ public class CrudInputFieldWrapper extends SimplePanel implements HasChangeHandl
 	 * Adds a change handler to this wrapper. Will proxy to underlying widget if the widget iimplements {@link HasChangeHandlers}. 
 	 * Otherwise a {@link UnsupportedOperationException} is thrown. 
 	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public HandlerRegistration addChangeHandler(ChangeHandler handler) {
+	public HandlerRegistration addChangeHandler(final ChangeHandler handler) {
 		if (widget instanceof HasChangeHandlers) {
 			return ((HasChangeHandlers) widget).addChangeHandler(handler);
+		}
+		if (widget instanceof HasValueChangeHandlers) {
+			return ((HasValueChangeHandlers)widget).addValueChangeHandler(new ValueChangeHandler<Object>() {
+				
+				@Override
+				public void onValueChange(ValueChangeEvent<Object> event) {
+					handler.onChange(null);
+				}
+			});
 		}
 		throw new UnsupportedOperationException("Field of type " + widget.getClass().getName() 
 				+ " does not implement " + HasChangeHandlers.class.getName());
